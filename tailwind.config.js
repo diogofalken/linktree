a7be89d30df820d64c71b5f74bb3481065693c88
diogofalken <diogofalken0@gmail.com>
2021-02-19 20:24:57 +0000

    build: added TailwindCSS, PostCSS, Vite, Yarn
    Prepared project with yarn.
    Added TailwindCSS, PostCSS and Vite.
    Added config files for
    TailwindCSS and PostCSS.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0B032D'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
