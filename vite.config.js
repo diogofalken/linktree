eed135b853cf29fa884fc5b6860fa0e58e38223f
diogofalken <diogofalken0@gmail.com>
2021-02-20 16:42:58 +0000

    refactor: changed stuff for PWA optimization

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default {
       manifest: {
         "name": "DC | Links",
         "short_name": "dlinks",
+	"start_url": "index.html",
         "orientation": "portrait",
         "theme_color": "#ffffff",
         "description": "All the links related to Diogo Costa.",
