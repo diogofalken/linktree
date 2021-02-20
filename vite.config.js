c38b4c1a5d9d502c3fcf7520b2ab7608c46f80c1
diogofalken <diogofalken0@gmail.com>
2021-02-20 16:52:15 +0000

    revert: revert previous commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default {
       manifest: {
         "name": "DC | Links",
         "short_name": "dlinks",
-	      "start_url": "/",
         "orientation": "portrait",
         "theme_color": "#ffffff",
         "description": "All the links related to Diogo Costa.",
